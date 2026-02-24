9d4b4b99d0bf9d2197a3d19120c7fc2134892392
Tharcisiio <souzalimeiratharcisio@gmail.com>
2026-02-24 11:38:51 -0300

    24/02/26

---
+++ aula01/script.js
@@ -0,0 +1,17 @@
+var nome = "Tharcísio";
+var idade = 15;
+var altura = 1.82;
+
+console.log( nome );
+console.log( idade );
+console.log( altura );
+
+document.write("<h1>Olá, sejam bem vindos ao JavaScript</h1>")
+
+document.write( nome +"<br>" );
+document.write( idade +"<br>" );
+document.write( altura +"<br>" );
+
+var dias = idade * 365;
+
+document.write("<p style='color : red'>Você já viveu " + dias + " dias </p>")
