019d64c9d22b996edcfcc305695714eefb479fb9
Tharcisiio <souzalimeiratharcisio@gmail.com>
2026-03-10 11:39:34 -0300

    10/03/26

---
+++ aula01/exercicio4.js
@@ -0,0 +1,8 @@
+document.write("<h1>Exercício 4</h1>");
+
+var base = Number (prompt("Digite o valor da base:"));
+var altura = Number (prompt("Digite o valor da altura:"))
+
+var resultado = ((base * altura)/2)
+document.write("<p>o valor da área é de " + resultado + "</p>");
+
---
+++ aula01/exercicio5.js
@@ -0,0 +1,7 @@
+document.write("<h1>Exercício 5</h1>");
+
+var lado = Number (prompt("Digite o lado do quadrado"));
+
+var resultado = (lado*lado)
+
+document.write("<p>o valor da área é de " + resultado + "</p>");
