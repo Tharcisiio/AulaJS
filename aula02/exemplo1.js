6278fac90ef1e40a35feefac0be4e17b36681c74
Tharcisiio <souzalimeiratharcisio@gmail.com>
2026-03-17 11:39:50 -0300

    17/03/26

---
+++ aula02/exemplo1.js
@@ -0,0 +1,16 @@
+function somar2numeros()
+{
+
+    let numero1, numero2, soma;
+
+    numero1 = document.getElementById("numero1").
+
+
+
+
+
+
+
+    document.getElementById("resultado").innerHTML = "A soma dos números é " + soma;
+}
+   
